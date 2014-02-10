0c59672a1475cb4ab87703afa92c8141e4db3343
James Abdul <james.abdul@craftturf.com>
2014-02-10 11:03:14 +0000

    Removing unused files.

--- src/module-appland/controller/index-controller.js
+++ src/module-appland/controller/index-controller.js
@@ -6,7 +6,7 @@ define([
 function (App, Doc, SimpleSidebarTmpl) {
   function IndexController() {
     /**
-     * App's DOM Container Element. 
+     * App's DOM Container Element.
      * @type {Object}
      */
     var appContainerEl = App.$('#apl-content');
@@ -16,59 +16,24 @@ function (App, Doc, SimpleSidebarTmpl) {
      * @returns {undefined}
      */
     function init() {
-      renderView(setRoutes);
+      renderView();
       delegateEvents();
-    }
-    function setRoutes() {
-      return App.Routes.extend({
-        /* Default constructor properties */
-      },{
-        /* Prototype properties and methods */
-        init: function() {
-          this.setRoutes();
-        },
-        setRoutes: function(){
-          var route = this.Route.addRoute('{/}');
-          route.matched.add(function(){
-          });
-          this.parseHash();
-        }
-      });
-      /*// String pattern or Regular Expression that
-      // should be used to match against requests.
-      App.Route.addRoute('{/}', function(section){
-        // For each route that matches, execute as follows:
-        //console.log(section);
-      });
-      // Updates location.hash of the current page.
-      App.Hash.setHash('dashboard');*/
     }
     /**
      * Renders the view templates.
      * @returns {undefined}
      */
-    function renderView(callBack) {
+    function renderView() {
       var templates = [
         SimpleSidebarTmpl(null)
       ];
       appContainerEl.html(templates.join('\n'));
-
-      var routes = new (callBack())();
-      routes.init();
     }
     /**
      * Event delegation.
      * @returns {undefined}
      */
     function delegateEvents() {
-      /*$(Doc)
-        .on("click.apl.job",
-          '#wgsn-promo-intro-close', function (e) {
-          e.preventDefault();
-        })
-        .on("change.apl.job",
-          '#wgsn-promo-never-show-intro', function (e) {
-        });*/
     }
 
     var publicMethods = {
