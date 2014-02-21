1747f87a7cc41d4e5850c5aeba6cc16b1857ab90
James Abdul <james.abdul@craftturf.com>
2014-02-21 13:54:56 +0000

    Correcting some formatting issues.

--- src-test/module-appland/main.js
+++ src-test/module-appland/main.js
@@ -1,21 +1,21 @@
 define([
-  'main',
+  'main'
 ],
-  function (App) {
-    /**
-     * Article's test suite.
-     */
-    describe('ModuleAppland: main (App)', function () {
+function (App) {
+  /**
+   * Article's test suite.
+   */
+  describe('ModuleAppland: main (App)', function () {
 
-      var that = this;
+    var that = this;
 
-      it("to be an Object literal", function () {
-        expect(App).toEqual(jasmine.any(Object));
-      });
+    it("to be an Object literal", function () {
+      expect(App).toEqual(jasmine.any(Object));
+    });
 
-      it("has jquery loaded", function () {
-        expect(App.$).toBeDefined();
-        expect(App.$).toEqual(jasmine.any(Function));
-      });
+    it("has jquery loaded", function () {
+      expect(App.$).toBeDefined();
+      expect(App.$).toEqual(jasmine.any(Function));
     });
   });
+});
