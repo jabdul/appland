776a337a854e722ddb67450c6a7091ebad39f303
James Abdul <james.abdul@craftturf.com>
2014-02-26 19:56:39 +0000

    Implemented the new struture for admin.

--- src/module-appland/nls/conf.js
+++ src/module-appland/nls/conf.js
@@ -4,7 +4,17 @@ define({
       "projectName": 'Project Name',
       "nameSpaces":['apl-'],
       "labels": [{
-        "LABEL_0": "Appland"
+        "LABEL_0": "Appland",
+        "LABEL_1": "Overview",
+        "LABEL_2": "Statistics overview",
+        "LABEL_3": "Modules",
+        "LABEL_4": "Active Modules",
+        "LABEL_5": "Unit Tests",
+        "LABEL_6": "Passed",
+        "LABEL_7": "Failed",
+        "LABEL_8": "Builds",
+        "LABEL_9": "Active",
+        "LABEL_10": "Module ID"
       }],
       "pathToAssets": "../"
     }
