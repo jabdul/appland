c98d61fe655cbe0cf049019a3084b04fce00b25a
James Abdul <james.abdul@craftturf.com>
2014-03-12 19:59:20 +0000

    Adding comments to jquery-loader.js

--- src/core/jquery-loader.js
+++ src/core/jquery-loader.js
@@ -1,6 +1,27 @@
-/**
- * see http://stackoverflow.com/questions/4858431/use-requirejs-and-jquery-without-clobbering-global-jquery
- */
+// **jquery-loader.js** is a simple wrapper to solve the problem of having multiple jQuery on the same site.
+// This solution prevents the 'clobbering' of existing global jQuery using AMD to encapsulate your preferred
+// version of jQuery library. This could be useful in some projects whereby removing the existing jQuery is
+// not an option.
+//
+// ## Usage
+//
+// `jquer-loader.js` is normally requested in your module's `common.js` configuration file:
+//
+// ```js
+// requirejs.config({
+//    baseUrl: '../../',
+//    waitSeconds: 14,
+//    paths: {
+//      'jquery': 'core/jquery-loader' /* See file for version. */
+//        ...
+//    }
+//    ...
+//  })
+// ```
+//
+// See an example here in the demo [demo-backbone](https://github.com/jabdul/appland/blob/demo/backbone/src/module-demo-backbone/common.js)
+// and [stackoverflow] (http://goo.gl/FhRZZW) for more on this issue.
+//
 define('jquery', ['lib/jquery-1-9-1/jquery'], function () {
   return jQuery.noConflict(true);
 });
