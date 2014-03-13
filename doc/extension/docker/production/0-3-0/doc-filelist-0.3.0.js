bf64340e0bc08d5e24f99e8502822d3ea0a5cfe9
James Abdul <james.abdul@craftturf.com>
2014-03-13 12:27:32 +0000

    Practise release of doc-0.3.0

---
+++ doc/extension/docker/production/0-3-0/doc-filelist-0.3.0.js
@@ -0,0 +1,1 @@
+var tree={"dirs":{"core":{"files":["connect.js","jquery-loader.js","util.js"]},"demos":{"files":["Backbone.md"]},"manual":{"files":["About.md","Installation.md","License-MIT.md","Requirements.md"]}},"files":["main.js"]};
